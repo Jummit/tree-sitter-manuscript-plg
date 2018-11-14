f7a68dbac7860c07fbd6bf740d905f3af7d2d6a8
Thomas Weber <thomas.weber@notengrafik.com>
2018-11-14 10:05:29 +0100

    Support function name highlighting and "docstrings"

--- grammar.js
+++ grammar.js
@@ -1,3 +1,10 @@
+const PREC = {
+  DEF: 1,
+  FUNCDEF: 2,
+  DOCDEF: 3,
+};
+
+
 module.exports = grammar({
   name: 'sibelius_plg',
 
@@ -7,29 +14,44 @@ module.exports = grammar({
 
   rules: {
     data: $ => seq(
-      optional('\uFEFF'), // UTF-16 BOM
+      optional('\uFEFF'),                // UTF-16 BOM
       choice(
-        $.table,          // .plg files
-        repeat($.def)     // GLOBALS.mss and .msd files
+        $.table,                         // .plg files
+        repeat(choice($.def, $.doc_def, $.function_def))  // GLOBALS.mss and .msd files
       )
     ),
 
     table: $ => seq(
       '{',
-      repeat(choice($._quoted_value, $.def, $.table)),
+      repeat(choice($._quoted_value, $.def, $.doc_def, $.function_def, $.table)),
       '}'
     ),
 
-    def: $ => prec.right(seq(
-      $.name,
+    def: $ => prec.right(PREC.DEF, seq(
+      choice($.label, alias($.doclabel, $.label)),
       optional(choice(
         seq(optional($._quoted_value), $.table),
         $._quoted_value,
       ))
     )),
 
-    name: $ => /[a-zA-Z0-9/.\-_]+/,
+    doc_def: $ => prec.right(PREC.DEF, seq(
+      $.doclabel, $.docstring, optional($.table)
+    )),
+
+    function_def: $ => prec.right(PREC.FUNCDEF, seq(
+      alias($.label, $.function_label), $._quoted_function, optional($.table)
+    )),
+
+    label: $ => /[a-zA-Z0-9.\-_][a-zA-Z0-9/.\-_]*/,
+    doclabel: $ => /\/[a-zA-Z0-9/.\-_]*/,
+    docstring: $ => /"[^"]*"/,
     _quoted_value: $ => seq('"', optional($.value), '"'),
-    value: $ => /[^"]+/,
+    value: $ => choice(/[^(][^"]*/, /\([^{"]*/),
+	// The approach of not including the quotes in the _quoted_function makes it
+    // possible to inject function syntax highlighting, which is defined without
+    // the quotes:
+    _quoted_function: $ => seq('"', $.function, '"'),
+    function: $ => /\([^"]*{[^"]*/,
   }
 });
