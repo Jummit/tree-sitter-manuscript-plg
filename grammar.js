351d1852e0515c6abb4f20d9f02b529215a71f2e
Thomas Weber <thomas.weber@notengrafik.com>
2018-12-15 14:28:21 +0100

    Support empty values
    Escaped quotes are in the works, but not ready yet

--- grammar.js
+++ grammar.js
@@ -4,6 +4,18 @@ const PREC = {
   DOCDEF: 3,
 };
 
+function stringStartingWith(start, pattern) {
+  if (!pattern) {
+    pattern = /[^"]+/;
+  }
+  return seq(
+    token.immediate(start),
+    optional(repeat(choice(
+      token.immediate(pattern),
+      token.immediate('""')
+    )))
+  );
+}
 
 module.exports = grammar({
   name: 'sibelius_plg',
@@ -23,15 +35,15 @@ module.exports = grammar({
 
     table: $ => seq(
       '{',
-      repeat(choice($._quoted_value, $.def, $.doc_def, $.function_def, $.table)),
+      repeat(choice($._quoted_value, $.empty_value, $.def, $.doc_def, $.function_def, $.table)),
       '}'
     ),
 
     def: $ => prec.right(PREC.DEF, seq(
       choice($.label, alias($.doclabel, $.label)),
       optional(choice(
-        seq(optional($._quoted_value), $.table),
-        $._quoted_value,
+        seq(optional(choice($._quoted_value, $.empty_value)), $.table),
+        choice($._quoted_value, $.empty_value)
       ))
     )),
 
@@ -46,9 +58,14 @@ module.exports = grammar({
     label: $ => /[a-zA-Z0-9.\-_][a-zA-Z0-9/.\-_]*/,
     doclabel: $ => /\/[a-zA-Z0-9/.\-_]*/,
     docstring: $ => /"[^"]*"/,
-    _quoted_value: $ => seq('"', optional($.value), '"'),
-    value: $ => choice(/[^(][^"]*/, /\([^{"]*/),
-	// The approach of not including the quotes in the _quoted_function makes it
+    empty_value: $ => '""',
+    _quoted_value: $ => seq('"', $.value, '"'),
+    value: $ => choice(
+      stringStartingWith(/[^("]/),
+      stringStartingWith(/[^"]/, /\([^{"]*/),
+      stringStartingWith('""')
+    ),
+    // The approach of not including the quotes in the _quoted_function makes it
     // possible to inject function syntax highlighting, which is defined without
     // the quotes:
     _quoted_function: $ => seq('"', $.function, '"'),
